9a5abd0fd1c7d60cbb05a9ade1d2896d1e04138f
Yosephus Viand <yosephzviand@gmail.com>
2021-07-29 21:13:32 +0700

    api

---
+++ app/Http/Controllers/API/QrcodeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Jimpitan;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class QrcodeController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data           =   new Jimpitan;
+        $data->warga    =   $request->warga;
+        $data->tanggal  =   Carbon::now();
+        $data->bulan    =   date('m');
+        $data->user     =   $request->iduser;
+        $data->save();
+
+        $response['meta']["status"]     =   200;
+        $response['meta']["message"]    =   "OK";
+        $response["response"] =
+        [
+            "warga"              =>  $request->warga,
+            "user"               =>  $request->iduser,
+            "tangal"             =>  Carbon::now(),
+        ];
+
+        return response()->json($response, $response['meta']["status"]);
+    }
+}
