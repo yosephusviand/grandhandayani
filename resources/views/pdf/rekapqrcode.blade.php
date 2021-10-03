33c98e8fd3cf76ef71c03d17ee9a777fa42dba7a
Yosephus Viand <yosephzviand@gmail.com>
2021-10-03 23:17:55 +0700

    sek

--- resources/views/pdf/rekapqrcode.blade.php
+++ resources/views/pdf/rekapqrcode.blade.php
@@ -7,36 +7,38 @@
 
 <body>
     <table width="100%" cellspacing="0" cellpadding="3" border="1" style="font-family: Courier New; font-size: 10pt">
-        <thead>
-            @foreach ( $data as $val)
+        @for ($i = 0; $i < count($data); $i++)
             <tr>
                 <th>
-                    <div class="form-group" style="text-align: center">
-                        <img src="data:image/png;base64,{{  base64_encode(QrCode::format('svg')->size(200)->errorCorrection('H')->generate($val->id)) }}}">
-                    </div>
-                    <div class="form-group" style="text-align: center; font-size: 20px">
-                        {{ $val->to_rumah->nama }}-{{ $val->norumah }} {{ $val->nama }}
-                    </div>
-                    
+                    @if ($i % 2 === 0)
+                        {{ $i }}
+                        <div class="form-group" style="text-align: center">
+                            <img
+                                src="data:image/png;base64,{{ base64_encode(QrCode::format('svg')->size(100)->errorCorrection('H')->generate($data[$i]->id),) }}}">
+                        </div>
+                        <div class="form-group" style="text-align: center; font-size: 20px">
+                            {{ $data[$i]->to_rumah->nama }}-{{ $data[$i]->norumah }}
+                            {{ $data[$i]->nama }}
+                        </div>
+
+                    @endif
                 </th>
-                {{-- <th></th>
                 <th>
-                    <div class="form-group" style="text-align: center">
-                        <img src="data:image/png;base64,{{  base64_encode(QrCode::format('svg')->size(300)->errorCorrection('H')->generate($val->id)) }}}">
-                    </div>
-                    <div class="form-group" style="text-align: center; font-size: 20px">
-                        {{ $val->to_rumah->nama }}-{{ $val->norumah }} {{ $val->nama }}
-                    </div>
-                    
-                </th> --}}
+                    @if ($i % 2 === 1)
+
+                        {{ $i }}
+                        <div class="form-group" style="text-align: center">
+                            <img
+                                src="data:image/png;base64,{{ base64_encode(QrCode::format('svg')->size(100)->errorCorrection('H')->generate($data[$i]->id),) }}}">
+                        </div>
+                        <div class="form-group" style="text-align: center; font-size: 20px">
+                            {{ $data[$i]->to_rumah->nama }}-{{ $data[$i]->norumah }}
+                            {{ $data[$i]->nama }}
+                        </div>
+                    @endif
+                </th>
             </tr>
-            <tr>
-                <th style="height: 10px"></th>
-                {{-- <th></th>
-                <th></th> --}}
-            </tr>
-            @endforeach
-        </thead>
+        @endfor
     </table>
 
 </body>
