5413f44f684dbd820566e3a3bb80df0fc6215242
Yosephus Viand <yosephzviand@gmail.com>
2021-10-11 16:21:47 +0700

    s

--- app/Http/Controllers/JimpitanController.php
+++ app/Http/Controllers/JimpitanController.php
@@ -18,7 +18,7 @@ class JimpitanController extends Controller
         $tanggal    =   $request->taggal ?? date('Y-m-d');
         $warga      =   Warga::whereNotIn('id', Jimpitan::select('warga')->where('tanggal', $tanggal))->get();
 
-        $jimpitan   =   Jimpitan::orderBy('id','desc')->all();
+        $jimpitan   =   Jimpitan::orderBy('id','desc')->get();
         return view('admin.jimpitan', compact('warga', 'jimpitan','tanggal'));
     }
 
