e8f4663629d64e3fa35a1f4edaa91f48e9d6a154
Yosephus Viand <yosephzviand@gmail.com>
2021-11-10 11:13:20 +0700

    add server key

--- vendor/apility/laravel-fcm/config/fcm.php
+++ vendor/apility/laravel-fcm/config/fcm.php
@@ -5,8 +5,8 @@ return [
     'log_enabled' => false,
 
     'http' => [
-        'server_key' => env('FCM_SERVER_KEY', 'Your FCM server key'),
-        'sender_id' => env('FCM_SENDER_ID', 'Your sender id'),
+        'server_key' => env('FCM_SERVER_KEY', 'AAAAv6BnKkA:APA91bGczQPD1Ir7Z8Zd7MXki8QfmyAWi5H99wfT8aKf2REuuLtb3ieNzoYGXI3P1o75KGBbYxHWqM5j21cBu2YVwstbRxauelnf1-UNZ6g_5PGqdAILKqrb01CLOwvj8ohGmvmQTlsB'),
+        'sender_id' => env('FCM_SENDER_ID', '823029869120'),
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
