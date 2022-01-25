28755460ccff50eed4bad8b0ddd5fbff3ec448b2
Yosephus <yosephzviand@gmail.com>
2022-01-25 19:15:31 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,14 @@ Route::post('login-update', [\App\Http\Controllers\API\UserController::class, 'l
 Route::post('register', [\App\Http\Controllers\API\UserController::class, 'register']);
 
 Route::group(['middleware' => 'auth:api'], function () {
-    // Route::get('ronda', [\App\Http\Controllers\API\UserController::class, 'ronda']);
-    // Route::get('home', [\App\Http\Controllers\API\UserController::class, 'home']);
-    // Route::get('user/{id}', [\App\Http\Controllers\API\UserController::class, 'user']);
-    // Route::get('userjimpitan/{id}', [\App\Http\Controllers\API\UserController::class, 'userjimpitan']);
-    // Route::get('user/detail', [\App\Http\Controllers\API\UserController::class, 'details']);
-    // Route::post('logout', [\App\Http\Controllers\API\UserController::class, 'logout']);
-    // Route::post('jimpitan',[\App\Http\Controllers\API\QrcodeController::class,'store']);
-    // Route::post('storetoken', [\App\Http\Controllers\API\UserController::class, 'storetoken']);
+    Route::get('ronda', [\App\Http\Controllers\API\UserController::class, 'ronda']);
+    Route::get('home', [\App\Http\Controllers\API\UserController::class, 'home']);
+    Route::get('user/{id}', [\App\Http\Controllers\API\UserController::class, 'user']);
+    Route::get('userjimpitan/{id}', [\App\Http\Controllers\API\UserController::class, 'userjimpitan']);
+    Route::get('user/detail', [\App\Http\Controllers\API\UserController::class, 'details']);
+    Route::post('logout', [\App\Http\Controllers\API\UserController::class, 'logout']);
+    Route::post('jimpitan',[\App\Http\Controllers\API\QrcodeController::class,'store']);
+    Route::post('storetoken', [\App\Http\Controllers\API\UserController::class, 'storetoken']);
     
     Route::get('ronda-update', [\App\Http\Controllers\API\UserController::class, 'ronda']);
     Route::get('home-update', [\App\Http\Controllers\API\UserController::class, 'home']);
