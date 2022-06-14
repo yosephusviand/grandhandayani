cf977e15e0a03bcb30458443b998179eb2be0871
Yosephus <yosephzviand@gmail.com>
2022-06-14 17:24:40 +0700

    ms

--- app/Http/Controllers/API/MobsiController.php
+++ app/Http/Controllers/API/MobsiController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class MobsiController extends Controller
 {
-    
+
     public $successStatus = 200;
 
     public function login()
@@ -24,26 +24,20 @@ class MobsiController extends Controller
 
     public function curlget(Request $request)
     {
-        $curl   = curl_init();
-        $url    = "https://mpresensi.gunungkidulkab.go.id/api/index.php/cekdevice/". $request->devid;
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_FAILONERROR, true);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        // curl_setopt_array($curl, array(
-        //     CURLOPT_URL => "https://mpresensi.gunungkidulkab.go.id/api/index.php/cekdevice/". $request->devid,
-        //     CURLOPT_RETURNTRANSFER => true,
-        //     CURLOPT_ENCODING => "",
-        //     CURLOPT_TIMEOUT => 30000,
-        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        //     CURLOPT_CUSTOMREQUEST => "GET",
-        //     CURLOPT_SSL_VERIFYPEER => false,
-        //     CURLOPT_HTTPHEADER => array(
-        //         // Set Here Your Requesred Headers
-        //         'Content-Type: application/json',
-        //     ),
-        // ));
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://mpresensi.gunungkidulkab.go.id/api/index.php/cekdevice/'. $request->devid,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+        ));
 
         // curl_setopt_array($curl, array(
         //     CURLOPT_URL => "https://mpresensi.gunungkidulkab.go.id/api/index.php/presensi/"+nip+"/"+tipe+"/"+latlon+"/"+jarak+"/-/"+token,
@@ -67,7 +61,6 @@ class MobsiController extends Controller
         $err = curl_error($curl);
         curl_close($curl);
         if ($err) {
-            // echo "cURL Error #:" . $err;
             return response($err);
         } else {
             $data = json_decode($response, true);
